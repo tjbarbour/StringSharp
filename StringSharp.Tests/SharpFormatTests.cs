f9d3795281cef34be94db6a40638acea2a95c13b
T.J. Barbour <tjbarbour@gmail.com>
2012-04-04 23:56:14 -0700

    Added BASIC test, initial class as well

---
+++ StringSharp.Tests/SharpFormatTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringSharp;
+
+namespace StringSharp.Tests
+{
+    [TestClass]
+    public class SharpFormatTests
+    {
+        [TestMethod]
+        public void ShouldLeaveConstantStringAlone()
+        {
+            string original = "Hello StringSharp!";
+            string formatted = original.SFormat();
+            Assert.AreEqual(original, formatted);
+        }
+    }
+}
---
+++ StringSharp/SharpFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringSharp
+{
+    public static class SharpFormat
+    {
+        public static string SFormat(this string str, params object[] args)
+        {
+            return String.Format(str, args);
+        }
+    }
+}
